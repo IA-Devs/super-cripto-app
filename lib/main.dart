155afb19fee51bafb919d6c06f30603dbc8b1e30
IsaiasArza <arza.isaias@gmail.com>
2025-01-20 10:18:34 -0300

    Add Firebase integration and update bundle identifier to version 2

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:super_cripto_app/config/router/app_router.dart';
 import 'package:super_cripto_app/config/theme/app_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MainApp());
 }
 
